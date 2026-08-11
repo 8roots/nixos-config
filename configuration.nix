95432824a913b7810565a5a1fdcfac5f1ea5c5e0
8roots <8roots@proton.me>
2026-08-11 12:14:08 +0000

    Add initial NixOS-WSL configuration

---
+++ configuration.nix
@@ -0,0 +1,46 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+
+  networking.proxy = {
+  default = "http://127.0.0.1:10808";
+
+  noProxy = "127.0.0.1,localhost,192.168.*,10.*,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,172.28.*,172.29.*,172.30.*,172.31.*";
+  };
+
+  environment.systemPackages = with pkgs; [
+  git
+  ripgrep
+  fd
+  fzf
+  jq
+  tree
+  btop
+
+];
+
+  
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+}
+
+
